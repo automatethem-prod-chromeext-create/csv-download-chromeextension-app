5a0a0d6bac10f4facbddd21072d6e45428df4274
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-08-05 15:35:41 +0900

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,19 @@
+console.log("I am popup.js");
+
+const button = document.querySelector('#button');
+//const button = document.getElementById("button");
+button.addEventListener("click", async () => {
+  var csvContent = "Name,Url,Description\n";
+  csvContent += "Name1,Url1,\"Description1\"\n";
+  csvContent += "Name2,Url2,\"Description2\"\n";
+  csvContent += "Name3,Url3,\"Description3-1\"\"Description3-2\"\"\"\n";
+
+  const downloadLink = document.createElement("a");
+  const dataBlob = new Blob([csvContent], { type: "text/csv;charset=utf-8" });
+  downloadLink.href = window.URL.createObjectURL(dataBlob);
+  downloadLink.download = "data.csv";
+  downloadLink.style.display = "none";
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+});
