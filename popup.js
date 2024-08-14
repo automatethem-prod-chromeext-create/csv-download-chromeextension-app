231d863f9c2346d19aed89307d81c9980c6acb13
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2024-08-14 16:27:42 +0900

    Add files via upload

--- popup.js
+++ popup.js
@@ -3,9 +3,9 @@ console.log("I am popup.js");
 const button = document.querySelector('#button');
 button.addEventListener("click", async () => {
   var csvContent = "Name,Url,Description\n";
-  csvContent += "Name1,Url1,\"Description1\"\n";
-  csvContent += "Name2,Url2,\"Description2\"\n";
-  csvContent += "Name3,Url3,\"Description3-1\"\"Description3-2\"\"\"\n";
+  csvContent += "Name1,Url1,\"한글내용1\"\n";
+  csvContent += "Name2,Url2,\"한글내용2\"\n";
+  csvContent += "Name3,Url3,\"한글내용3-1\"\"한글내용3-2\"\"\"\n";
 
   /*
   const downloadLink = document.createElement("a");
@@ -14,7 +14,7 @@ button.addEventListener("click", async () => {
   const dataBlob = new Blob([BOM + csvContent], { type: "text/csv;charset=utf-8" });
   //const dataBlob = new Blob([csvContent], { type: "text/csv;charset=EUC-KR" });
   downloadLink.href = window.URL.createObjectURL(dataBlob);
-  downloadLink.download = "data.csv";
+  downloadLink.download = "outputs/data.csv"; //outputs_data.csv 로 저장됨
   downloadLink.style.display = "none";
   document.body.appendChild(downloadLink);
   downloadLink.click();
@@ -29,7 +29,7 @@ button.addEventListener("click", async () => {
   // chrome.downloads.download를 사용하여 파일을 다운로드합니다.
   const downloadId = await chrome.downloads.download({
     url: dataUrl,
-    filename: 'naver-band-data.csv',
+    filename: 'outputs/data.csv',
     //saveAs: true // 파일 저장 대화 상자 표시
   });
   //console.log(downloadId);
